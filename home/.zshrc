709612d743bc26e78f135d4d2804bee6869f6654
Jack Blanc <jackblanc0@proton.me>
2024-09-25 02:23:19 -0400

    More fixes

--- home/.zshrc
+++ home/.zshrc
@@ -5,7 +5,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
@@ -36,4 +36,7 @@ export VISUAL='nvim'
 export ANTHROPIC_API_KEY="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"
 
 export STRIPE_API_KEY="sk_test_51PIlJQJ48Myylpy8ljR6N9azZ392E9SWYFe6dSEwVaSBMAveyQXELnJusnahRkH5yrZ46W8CGTfaMA1q83dmFz3v00wYqzPDKc"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
--- setup.sh
+++ setup.sh
@@ -1,12 +1,27 @@
+#!/bin/zsh
 
-#!/bin/bash
+echo $(pwd)
+echo $HOME
 
-rm -rf ~/.oh-my-zsh/
-rm .zshrc
+rm -rf $HOME/.oh-my-zsh/
+rm $HOME/.zshrc
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Prevent the installer from replacing the shell
+RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install zsh-syntax-highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cp $HOME/environment/home/.zshrc $HOME/.zshrc
 
 
+source $HOME/.zshrc
 
