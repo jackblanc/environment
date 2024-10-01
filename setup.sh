70a372c7495a90f7a2bfaccfbb39eb440592375f
Jack Blanc <jackblanc0@proton.me>
2024-10-01 00:08:52 -0400

    add brew

--- setup.sh
+++ setup.sh
@@ -46,3 +46,17 @@ done
 
 source $HOME/.zshrc
 
+# Check if Homebrew is already installed
+if command -v brew >/dev/null 2>&1; then
+    echo "Homebrew is already installed."
+else
+    echo "Installing Homebrew..."
+
+    # Use non-interactive installation of Homebrew to avoid overwriting existing files
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    echo "Homebrew installation complete."
+fi
+
+brew install lazygit bun gh mkcert ripgrep
+
