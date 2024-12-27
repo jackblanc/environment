2a087911d81fdc584b6ea31e54d0cf6c1cba09f6
Jack Blanc <jackblanc0@proton.me>
2024-12-26 17:07:21 -0800

    Improve setup script, add ghostty config

--- setup.sh
+++ setup.sh
@@ -12,32 +12,59 @@ link() {
 	ln -s $SOURCE $DEST
 }
 
-# Remove existing .zshrc and .oh-my-zsh before installation
-rm -rf $HOME/.oh-my-zsh
-rm $HOME/.zshrc
+clone_or_pull() {
+    REPO_URL=$1
+    TARGET_DIR=$2
+    if [ -d "$TARGET_DIR" ]; then
+        echo "Updating $(basename $TARGET_DIR)..."
+        cd "$TARGET_DIR"
+        git pull
+        cd - > /dev/null
+    else
+        echo "Cloning $(basename $REPO_URL)..."
+        git clone --depth=1 "$REPO_URL" "$TARGET_DIR"
+    fi
+}
 
+# Install oh-my-zsh if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "Oh My Zsh is already installed"
+fi
 
-# Install oh-my-zsh and plugins
-RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions \
-  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git \
-  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/you-should-use
+# Remove existing .zshrc to ensure clean configuration
+rm -f $HOME/.zshrc
+
+# Install/update themes and plugins
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
+# Powerlevel10k theme
+clone_or_pull https://github.com/romkatv/powerlevel10k.git \
+    "$ZSH_CUSTOM/themes/powerlevel10k"
+
+# Plugins
+clone_or_pull https://github.com/zsh-users/zsh-autosuggestions \
+    "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+
+clone_or_pull https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+
+clone_or_pull https://github.com/MichaelAquilina/zsh-you-should-use.git \
+    "$ZSH_CUSTOM/plugins/you-should-use"
 
 # home 
 for file in $PWD/home/*(D); do
-	name=${file##*/}
-	link home/$name $name
+  name=${file##*/}
+  link home/$name $name
 done
 
 # configs
 mkdir -p $HOME/.config
 for file in $PWD/config/*; do
-	name=${file##*/}
-	link config/$name .config/$name
+  name=${file##*/}
+  link config/$name .config/$name
 done
 
 source $HOME/.zshrc
@@ -47,12 +74,19 @@ if command -v brew >/dev/null 2>&1; then
     echo "Homebrew is already installed."
 else
     echo "Installing Homebrew..."
-
     # Use non-interactive installation of Homebrew to avoid overwriting existing files
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
     echo "Homebrew installation complete."
 fi
 
-brew install lazygit oven-sh/bun/bun gh mkcert ripgrep neovim
+# Install or upgrade packages
+for package in lazygit oven-sh/bun/bun gh mkcert ripgrep neovim ghostty; do
+    if brew list $package &>/dev/null; then
+        echo "Upgrading $package..."
+        brew upgrade $package
+    else
+        echo "Installing $package..."
+        brew install $package
+    fi
+done
 
